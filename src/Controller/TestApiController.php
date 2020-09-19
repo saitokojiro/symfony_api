c056addb6af55e87ce670287c3d6d1cdb6dbc922
Saito Kojiro <kazutonight@gmail.com>
2020-09-19 23:48:53 +0200

    Api is create

--- src/Controller/TestApiController.php
+++ src/Controller/TestApiController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\GameRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -13,20 +14,79 @@ class TestApiController extends AbstractController
     public function index()
     {
 
-$data = array(
-        [
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TestApiController.php',
-        ],
-        [
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/TestApiController.php',
-        ]
-    );
+        $data = array(
+            [
+                'message' => 'Welcome to your new controller!',
+                'path' => 'src/Controller/TestApiController.php',
+            ],
+            [
+                'message' => 'Welcome to your new controller!',
+                'path' => 'src/Controller/TestApiController.php',
+            ],
+        );
 
-        
+        //return $this->json($data);
+        $article = file_get_contents(__DIR__ . '/../data/article.json');
+        $jsonD = json_decode($article, true);
+        return $this->json($jsonD);
 
-        return $this->json($data);
-        
     }
+
+    /**
+     * @Route("/games/api", name="games_api")
+     */
+
+    public function games(GameRepository $articleGm)
+    {
+        $data = array(
+            [
+                'message' => 'Welcome to your new controller!',
+                'path' => 'src/Controller/TestApiController.php',
+            ],
+            [
+                'message' => 'Welcome to your new controller!',
+                'path' => 'src/Controller/TestApiController.php',
+            ],
+        );
+
+        //return $this->json($data);
+        //$article = file_get_contents(__DIR__ . '/../data/article.json');
+
+        $article = $articleGm->findAll();
+
+        $jsonD = json_encode($article);
+
+        return $this->json($article);
+    }
+
+
+
+    /**
+     * @Route("/game/{id}/api", name="game_api")
+     */
+
+    public function gameid(GameRepository $articleGm, $id)
+    {
+        $data = array(
+            [
+                'message' => 'Welcome to your new controller!',
+                'path' => 'src/Controller/TestApiController.php',
+            ],
+            [
+                'message' => 'Welcome to your new controller!',
+                'path' => 'src/Controller/TestApiController.php',
+            ],
+        );
+
+        //return $this->json($data);
+        //$article = file_get_contents(__DIR__ . '/../data/article.json');
+
+        $article = $articleGm->findOneBySomeField($id);
+
+        $jsonD = json_encode($article);
+
+        return $this->json($article);
+    }
+
+
 }
---
+++ src/Entity/Game.php
@@ -0,0 +1,159 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=GameRepository::class)
+ */
+class Game implements \JsonSerializable
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $description;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $date;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $plateform;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $devise;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $img;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getPlateform(): ?string
+    {
+        return $this->plateform;
+    }
+
+    public function setPlateform(string $plateform): self
+    {
+        $this->plateform = $plateform;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
+    }
+
+    public function getDevise(): ?string
+    {
+        return $this->devise;
+    }
+
+    public function setDevise(string $devise): self
+    {
+        $this->devise = $devise;
+
+        return $this;
+    }
+
+    public function jsonSerialize()
+    {
+        // TODO: Implement jsonSerialize() method.
+        return [
+            "id" => $this->getId(),
+            "img" => $this->getImg(),
+            "name" => $this->getName(),
+            "description" => $this->getDescription(),
+            "date" => $this->getDate()->format("Y"),
+            "platform" => $this->getPlateform(),
+            "price" => $this->getPrice(),
+            "devise" => $this->getDevise(),
+
+        ];
+    }
+
+    public function getImg(): ?string
+    {
+        return $this->img;
+    }
+
+    public function setImg(string $img): self
+    {
+        $this->img = $img;
+
+        return $this;
+    }
+}
